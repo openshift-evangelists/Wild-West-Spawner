ec2318d48d70db3681c2ecd3f02bdb6dd62dca7b
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2018-08-21 10:41:00 +1000

    Initial files.

---
+++ jupyterhub/.jupyter/jupyterhub_config.py
@@ -0,0 +1,20 @@
+import os
+
+c.KubeSpawner.cmd = ['/tmp/scripts/run']
+
+# Setup location for customised template files.
+
+c.JupyterHub.template_paths = ['/opt/app-root/src/templates']
+
+# Setup culling of idle notebooks if timeout parameter is supplied.
+
+idle_timeout = os.environ.get('JUPYTERHUB_IDLE_TIMEOUT')
+
+if idle_timeout and int(idle_timeout):
+    c.JupyterHub.services = [
+        {
+            'name': 'cull-idle',
+            'admin': True,
+            'command': ['cull-idle-servers', '--timeout=%s' % idle_timeout],
+        }
+    ]
