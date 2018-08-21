689d18a4cf4d90bd02a7cb6ee9816848eb6d1c1e
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2018-08-21 14:27:09 +1000

    Reorganise directories.

---
+++ .jupyter/jupyterhub_config.py
@@ -0,0 +1,54 @@
+import os
+
+# Override image details with that of the front end.
+
+c.KubeSpawner.singleuser_image_spec = os.environ.get(
+        'FRONTEND_IMAGE', 'wild-west-frontend:latest')
+
+c.KubeSpawner.cmd = ['/usr/libexec/s2i/run']
+
+c.KubeSpawner.pod_name_template = '%s-frontend-{username}' % (
+        c.KubeSpawner.hub_connect_ip)
+
+# Override URL prefix for front end instance and link to the back end.
+
+from openshift import client, config
+
+with open('/var/run/secrets/kubernetes.io/serviceaccount/namespace') as fp:
+    namespace = fp.read().strip()
+
+config.load_incluster_config()
+oapi = client.OapiApi()
+
+routes = oapi.list_namespaced_route(namespace)
+
+def extract_hostname(routes, name):
+    for route in routes.items:
+        if route.metadata.name == name:
+            return route.spec.host
+
+route_name = os.environ.get('ROUTE_NAME')
+route_hostname = extract_hostname(routes, route_name)
+
+def modify_pod_hook(spawner, pod):
+    pod.spec.containers[0].env.append(dict(name='URL_PREFIX',
+            value='/user/%s' % spawner.user.name))
+    pod.spec.containers[0].env.append(dict(name='BACKEND_SERVICE',
+            value='http://%s' % route_hostname))
+
+    return pod
+
+c.KubeSpawner.modify_pod_hook = modify_pod_hook
+
+# Setup culling of front end instance if timeout parameter is supplied.
+
+idle_timeout = os.environ.get('IDLE_TIMEOUT')
+
+if idle_timeout and int(idle_timeout):
+    c.JupyterHub.services = [
+        {
+            'name': 'cull-idle',
+            'admin': True,
+            'command': ['cull-idle-servers', '--timeout=%s' % idle_timeout],
+        }
+    ]
