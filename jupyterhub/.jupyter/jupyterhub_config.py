f7b7e1032f0e02202188546aa0076ed979f06323
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2018-08-21 11:45:05 +1000

    Link front end to backend.

--- jupyterhub/.jupyter/jupyterhub_config.py
+++ jupyterhub/.jupyter/jupyterhub_config.py
@@ -2,9 +2,17 @@ import os
 
 c.KubeSpawner.cmd = ['/usr/libexec/s2i/run']
 
-# Setup location for customised template files.
+# Override URL prefix for front end instance.
 
-c.JupyterHub.template_paths = ['/opt/app-root/src/templates']
+def modify_pod_hook(spawner, pod):
+    pod.spec.containers[0].env.append(dict(name='URL_PREFIX',
+            value='/user/%s' % spawner.user.name))
+    pod.spec.containers[0].env.append(dict(name='BACKEND_SERVICE',
+            value='http://wild-west-town-backend:8080/'))
+
+    return pod
+
+c.KubeSpawner.modify_pod_hook = modify_pod_hook
 
 # Setup culling of idle notebooks if timeout parameter is supplied.
 
